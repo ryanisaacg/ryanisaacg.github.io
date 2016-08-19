52a0d54ca587e968e327113463494410a84dae65
EpicPieGuy <ryanisaacg@gmail.com>
2016-08-19 12:47:02 -0400

    Switched to pure-Python generation

--- generate.py
+++ generate.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-import subprocess
+import markdown
 with open('style.css', 'r') as data:
     style = data.read()
-with open('content.html', 'w') as data:
-    subprocess.call(['markdown', 'content.md'], stdout = data)
-with open('content.html', 'r') as data:
-    content = data.read()
+with open('content.md', 'r') as data:
+    text = data.read()
 with open('frame.html', 'r') as data:
     frame = data.read()
 with open('index.html', 'w') as data:
-    data.write((frame % (style, content)).replace('  ', ' ').replace('\r', '').replace('\n', '').replace('\t', '').replace(', ', ',').replace(' {', '{'))
-
+    data.write((frame % (style, markdown.markdown(text)))
+    .replace('  ', ' ')
+    .replace('\r', '')
+    .replace('\n', '')
+    .replace('\t', '')
+    .replace(', ', ',')
+    .replace(' {', '{'))

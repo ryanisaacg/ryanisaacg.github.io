5ddc80c71f11b80bd93159d62a8ac94539f7d6da
Ryan Goldstein <ryanisaacg@gmail.com>
2017-04-02 11:33:07 -0400

    Added some javascript to try to add expand / collapse behavior

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,15 +1,20 @@
 var collapse_sections = document.getElementsByClassName('collapse')
 for(var i = 0; i < collapse_sections.length; i++) {
 	var section = collapse_sections[i];
+	var text = document.createTextNode("\u21E9");
+	text.style = {}
+	text.style.float = 'left'
+	section.children[0].appendChild(text)
 	section.onclick = function(e) {
-		var style = this.children[1].style;
+		var content = this.children[1];
+		var style = content.style;
 		console.log(style.display);
-		if(!this.children[1].open) {
+		if(!content.open) {
 			style.display = 'flex';
-			this.children[1].open = true;
+			content.open = true;
 		} else {
 			style.display = 'none';
-			this.children[1].open = false;
+			content.open = false;
 		}
 	};
 }

cb4bdb8b34704d14214bb12ededd5866413f4cee
Ryan Goldstein <ryanisaacg@gmail.com>
2017-10-31 00:35:53 -0400

    Removed the showcase and associated clutter

--- showcase/js/subpage.js
+++
@@ -1,7 +0,0 @@
-function gohome() {
-	if(document.referrer.indexOf("ryanisaacg.com") == -1 && document.referrer.indexOf("ryanisaacg.github.io") == -1) {
-		window.location.href = "../index.html";
-	} else {
-		window.history.back();
-	}
-}
